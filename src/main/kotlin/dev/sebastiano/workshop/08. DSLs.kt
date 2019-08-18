8fb55c8dd1c365ef2a0d4aff156b4b9133653518
Sebastiano Poggi <poggos@gmail.com>
2019-08-18 18:49:56 +0100

    Add exercise: 08. DSLs

---
+++ src/main/kotlin/dev/sebastiano/workshop/08. DSLs.kt
@@ -0,0 +1,64 @@
+package dev.sebastiano.workshop
+
+// TODO: create a DSL so that HtmlDslTest passes.
+//
+// Required DSL method names and expected toString output:
+//
+//      * html              -> <html>...</html>
+//        * head            -> <head>...</head>
+//          * title         -> <title>...</title>
+//        * body            -> <body>...</body>
+//          * h1            -> <h1>...</h1>
+//          * orderedList   -> <orderedList>...</orderedList>
+//            * listItem    -> <listItem>...</listItem>
+//              * rawText   -> [text value]
+//
+// Where each ... in a tag is essentially the toString() of all child nodes.
+// For example, the toString() of:
+//
+//      html { head { title("Title") } }
+//
+// Should be an equivalent to: <html><head><title>Title</title></head></html>
+// (the tests use JSoup to normalize the resulting HTML to give you some freedom)
+
+abstract class Tag(
+        private val name: String,
+        val children: MutableList<Tag> = mutableListOf()
+) {
+
+    override fun toString(): String {
+        fun <T> Collection<T>.joinToStringIfNotEmpty() = if (isNotEmpty()) joinToString(separator = " ") else ""
+        return "<$name>${children.joinToStringIfNotEmpty()}</$name>"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Tag
+
+        if (name != other.name) return false
+        if (children != other.children) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + children.hashCode()
+        return result
+    }
+}
+
+class Html : Tag("html")
+
+class Head : Tag("head")
+class Body : Tag("body")
+class Title(text: String) : Tag("title", children = mutableListOf(RawText(text)))
+class Header1(text: String) : Tag("h1", children = mutableListOf(RawText(text)))
+class OrderedList : Tag("ol")
+class ListItem : Tag("li")
+
+class RawText(private val text: String) : Tag("[raw text]") {
+    override fun toString(): String = text
+}
---
+++ src/test/kotlin/dev/sebastiano/workshop/08. DSLs Test.kt
@@ -0,0 +1,72 @@
+package dev.sebastiano.workshop
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.junit.jupiter.api.Test
+import javax.script.ScriptEngineManager
+
+internal class HtmlDslTest {
+
+    @Test
+    internal fun `should produce HTML with the right title in the head`() {
+        val document = evaluateAndParse(
+                """
+                html {
+                    head {
+                        title("The title")
+                    }
+                }.toString()
+                """.trimIndent()
+        )
+
+        assertThat(document.select("head title").text()).isEqualTo("The title")
+    }
+
+    @Test
+    internal fun `should produce HTML with the right h1 in the body`() {
+        val document = evaluateAndParse(
+                """
+                html {
+                    body {
+                        h1("The title")
+                    }
+                }.toString()
+                """.trimIndent()
+        )
+
+        assertThat(document.select("body h1").text()).isEqualTo("The title")
+    }
+
+    @Test
+    internal fun `should produce HTML with a properly rendered list in the body`() {
+        val document = evaluateAndParse(
+                """
+                html {
+                    body {
+                        h1("The title")
+                        orderedList {
+                            listItem { rawText("First item") }
+                            listItem { rawText("Second item") }
+                            listItem { /* Empty item */ }
+                            listItem { rawText("Third item") }
+                        }
+                    }
+                }.toString()    
+                """.trimIndent()
+        )
+
+        assertThat(document.select("body ol li:eq(0)").text()).isEqualTo("First item")
+        assertThat(document.select("body ol li:eq(1)").text()).isEqualTo("Second item")
+        assertThat(document.select("body ol li:eq(2)").hasText()).isFalse()
+        assertThat(document.select("body ol li:eq(3)").text()).isEqualTo("Third item")
+    }
+}
+
+private val ktsEngine = ScriptEngineManager(HtmlDslTest::class.java.classLoader).getEngineByExtension("kts")!!
+
+fun evaluateAndParse(snippet: String): Document = Jsoup.parse(
+        ktsEngine.eval("import dev.sebastiano.workshop.*\n\n$snippet")?.toString()
+)
